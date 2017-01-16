f3ba57577bf59de99821a1ab7310b3c6348d2d16
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-16 13:35:43 +0300

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,7 @@ import { HeaderComponent } from "./shared/header.component";
 import { SigninComponent } from "./unprotected/signin.component";
 import { SignupComponent } from "./unprotected/signup.component";
 import { ProtectedComponent } from "./protected/protected.component";
-import { SigninComponent } from './unprotected/signin.component';
-import { HeaderComponent } from './shared/header.component';
+import {routing} from "./app.routing";
 
 @NgModule({
   declarations: [
@@ -19,7 +18,11 @@ import { HeaderComponent } from './shared/header.component';
     SignupComponent,
     HeaderComponent
   ],
-  imports: [BrowserModule, HttpModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
+    routing],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
