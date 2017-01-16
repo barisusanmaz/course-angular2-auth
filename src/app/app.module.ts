42b58a5e3c7fdc5711b613a3008be82d898cdf1d
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-16 11:16:41 +0300

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { HeaderComponent } from "./shared/header.component";
+import { SigninComponent } from "./unprotected/signin.component";
+import { SignupComponent } from "./unprotected/signup.component";
+import { ProtectedComponent } from "./protected/protected.component";
+import { SigninComponent } from './unprotected/signin.component';
+import { HeaderComponent } from './shared/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProtectedComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  imports: [BrowserModule, HttpModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
